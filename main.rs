05739f55aab106a1fea2aaff77b516a7ba41a81f
Julia Evans <julia@jvns.ca>
2013-11-29 18:55:02 -0500

    Write a lot of 'c's to the screen

--- main.rs
+++ main.rs
@@ -2,6 +2,9 @@
 #[no_std];
 #[no_core];
 
+static VGA_WIDTH  : u16 = 80;
+static VGA_HEIGHT : u16 = 24;
+
 mod zero;
 
 enum Color {
@@ -33,11 +36,24 @@ fn range(lo: uint, hi: uint, it: &fn(uint)) {
 
 unsafe fn clear_screen(background: Color) {
     range(0, 80*25, |i| {
-        *((0xb8000 + i * 2) as *mut u16) = (background as u16) << 12;
+        *((0xb8000 + i * 2) as *mut u16) = make_vgaentry(99, Black, Red);
+                
     });
 }
 
+unsafe fn putchar(x: u16, y: u16, c: u8) {
+    let idx : u16 = y * VGA_WIDTH + x;
+    *((0xb8000 + idx) as *mut u16) = make_vgaentry(c, Black, Yellow);
+}
+
+fn make_vgaentry(c: u8, fg: Color, bg: Color) -> u16 {
+    let color = fg as u16 | (bg as u16 << 4);
+    return c as u16 | (color << 8);
+}
+
+
 #[no_mangle]
 pub unsafe fn main() {
-    clear_screen(LightRed);
+    clear_screen(White);
+    putchar(15, 15, 99);
 }
