19254e3a850ca1ebea72dad2c59cf124d67200d3
Julia Evans <julia@jvns.ca>
2013-12-03 12:31:20 -0500

    Make character-printing loop more readable

--- main.rs
+++ main.rs
@@ -16,11 +16,13 @@ pub unsafe fn main() {
     stdio::clear_screen(stdio::Green);
     stdio::write("Hello!aaa", 2, 3);
     let mut i: u32 = 0;
+    let mut c: u8 = 65; // 'A'
     let N: u32 = 80000000;
     while true {
         i += 1;
         if (i % N == 0) {
-            stdio::putchar(2, 4, 65 + ((i / N) as u8));
+            c += 1;
+            stdio::putchar(2, 4, c);
         }
     }
 }
