0db7a20d83f6f2046ecad9ed894a1cdbce8868d7
Julia Evans <julia@jvns.ca>
2013-12-04 16:03:55 -0500

    Try some different PIC initialization code

--- pic.rs
+++ pic.rs
@@ -28,28 +28,14 @@ arguments:
 */
 pub unsafe fn PIC_remap(offset1: u8, offset2: u8)
 {
-    
-    let a1 = inb(PIC1_DATA);           // save masks
-    let a2 = inb(PIC2_DATA);           // save masks
-    
     outb(PIC1_COMMAND, ICW1_INIT+ICW1_ICW4);  // starts the initialization sequence (in cascade mode)
-    io_wait();
     outb(PIC2_COMMAND, ICW1_INIT+ICW1_ICW4);
-    io_wait();
     outb(PIC1_DATA, offset1);                 // ICW2: Master PIC vector offset
-    io_wait();
     outb(PIC2_DATA, offset2);                 // ICW2: Slave PIC vector offset
-    io_wait();
     outb(PIC1_DATA, 4);                       // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
-    io_wait();
     outb(PIC2_DATA, 2);                       // ICW3: tell Slave PIC its cascade identity (0000 0010)
-    io_wait();
-    
     outb(PIC1_DATA, ICW4_8086);
-    io_wait();
     outb(PIC2_DATA, ICW4_8086);
-    io_wait();
-    
-    outb(PIC1_DATA, a1);   // restore saved masks.
-    outb(PIC2_DATA, a2);
+    outb(PIC1_DATA, 0x0);
+    outb(PIC2_DATA, 0x0);
 }
--- utils.rs
+++ utils.rs
@@ -1,3 +1,4 @@
+use stdio;
 pub unsafe fn outb(port: u16, value: u8) {
     asm!("outb %al, %dx"
          :
@@ -8,11 +9,14 @@ pub unsafe fn outb(port: u16, value: u8) {
 
 pub unsafe fn inb(port: u16) -> u8 {
     let mut ret: u8;
+    stdio::putc(66);
     asm!("inb %dx, %al"
          : "={al}"(ret)
          : "{dx}"(port)
          :
          : "volatile" );
+    stdio::putc(65);
+    stdio::putc(ret);
     return ret;
 }
 
