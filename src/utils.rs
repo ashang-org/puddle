2d32daa27305c93131145e63aff93fd1e4a599d4
Julia Evans <julia@jvns.ca>
2013-12-05 10:59:57 -0500

    Remove debug code from putc()

--- src/utils.rs
+++ src/utils.rs
@@ -9,14 +9,11 @@ pub unsafe fn outb(port: u16, value: u8) {
 
 pub unsafe fn inb(port: u16) -> u8 {
     let mut ret: u8;
-    stdio::putc(66);
     asm!("inb %dx, %al"
          : "={al}"(ret)
          : "{dx}"(port)
          :
          : "volatile" );
-    stdio::putc(65);
-    stdio::putc(ret);
     return ret;
 }
 
