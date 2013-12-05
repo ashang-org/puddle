1cd89d364a51523f36bf769727999226e02199d0
Julia Evans <julia@jvns.ca>
2013-12-05 12:49:05 -0500

    What is even going on

--- src/kbd.rs
+++ src/kbd.rs
@@ -1,12 +1,19 @@
 use utils::{outb, inb};
+use core::slice;
+use core::container::Container;
 use stdio;
 
+static NUMS: &'static [u8] = bytes!("01234567890");
 
 static mut pos: u8 = 0;
 #[no_mangle]
 pub unsafe fn _interrupt_handler_kbd() {
-    pos += 1;
+    let keycode = inb(0x60);
+    if (keycode == 2 || keycode == 3) {
+        stdio::putc(NUMS[2]); // should be '2'. It is
+        stdio::putc(65 + keycode); // should be 'C' (keycode = 2). It is. 
+        stdio::putc(NUMS[keycode]); // should be '2', BUT IT ISN'T
+        stdio::putc(124);
+    }
     outb(0x20, 0x20);
-    let key = inb(0x60);
-    stdio::putchar(5 + (pos as u16), 6, key);
 }
