1d6f1c592ab259c6e4563fded3027c3e077ac927
Julia Evans <julia@jvns.ca>
2013-12-08 20:40:24 -0500

    Make shift work

--- src/kbd.rs
+++ src/kbd.rs
@@ -1,7 +1,10 @@
 use utils::{outb, inb};
 use core::slice;
+use core::option::{Option, Some, None};
 use core::container::Container;
 use stdio;
+
+static mut shifted: bool = false;
 
 static SCAN_CODE_MAPPING: &'static [u8] = bytes!("\
 \x00\x1B1234567890-=\x08\tqwertyuiop[]\n?asdfghjkl;'`?\\zxcvbnm,./?*? ?????????????789-456+1230.?????");
@@ -9,12 +12,43 @@ static SCAN_CODE_MAPPING_SHIFTED: &'static [u8] = bytes!("\
 \x00\x1B!@#$%^&*()_+\x08\tQWERTYUIOP{}\n?ASDFGHJKL:\"~?|ZXCVBNM<>??*? ?????????????789-456+1230.?????");
 
 
-static mut pos: u8 = 0;
 #[no_mangle]
 pub unsafe fn _interrupt_handler_kbd() {
-    let keycode = inb(0x60);
-    if (keycode < SCAN_CODE_MAPPING.len() as u8) {
-        stdio::putc(SCAN_CODE_MAPPING[keycode]);
-    }
+    let scancode = inb(0x60);
+    change_state(scancode);
+    match get_char(scancode) {
+        None => {},
+        Some(c) => stdio::putc(c)
+    };
     outb(0x20, 0x20);
+}
+
+
+pub fn change_state(scancode: u8) {
+    let is_keydown: bool = scancode & 0x80 == 0;
+    if is_keydown {
+        match scancode {
+            0x2A | 0x36 => unsafe { shifted = true },
+            _ => {}
+        }
+    } else {
+        let scancode_lower = scancode & !0x80u8;
+        match scancode_lower {
+            0x2A | 0x36 => unsafe { shifted = false },
+            _ => {}
+        }
+    }
+}
+
+pub fn get_char(scancode: u8) -> Option<u8> {
+    if (scancode > SCAN_CODE_MAPPING.len() as u8) {
+        return None;
+    } 
+    unsafe {
+        if shifted {
+            Some(SCAN_CODE_MAPPING_SHIFTED[scancode])
+        } else {
+            Some(SCAN_CODE_MAPPING[scancode])
+        }
+    }
 }
